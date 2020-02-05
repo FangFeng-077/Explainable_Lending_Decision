a0de8301216d5d6b8f77b670409360b0cef25704
Fang Feng <fangfeng077@gmail.com>
2020-02-05 09:33:01 -0800

    Update Shap.py

--- src/explain/Shap.py
+++ src/explain/Shap.py
@@ -32,7 +32,6 @@ class Shap:
         shap.force_plot(self.expected_value,
                         self.shap_values[:index, :],
                         data,
-                        matplotlib=True,
                         link='logit')
 
     def get_shap_values(self):
